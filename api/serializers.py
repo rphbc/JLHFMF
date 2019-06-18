837c165a7dfce580eca7d96c5658cb4f84937896
Raphael Costa <raphaelbc@gmail.com>
2019-06-17 22:42:45 -0300

    initial commit

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+
+# Aqui ficam os serializers, eles ajudam quando você precisar usar banco de
+# daoos
+
+# class ExampleSerializer(serializers.Serializer):
+#     email = serializers.EmailField()
+#     content = serializers.CharField(max_length=200)
+#     created = serializers.DateTimeField()
---
+++ api/views.py
@@ -0,0 +1,36 @@
+import json
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class Return1(APIView):
+
+    def get(self, request):
+        return Response({'resultado': 1})
+
+    def post(self, request):
+        dados = request  # recebe os dados que o cara enviou
+        json_data = json.loads(
+            dados.data)  # faz o parse de json para dicionario
+        # Aqui você pode fazer algo interessante com o dado, tipo salvar no db
+        return Response({'resultado': json_data})
+
+
+return1 = Return1.as_view()
+
+
+class Return2(APIView):
+
+    def get(self, request):
+        return Response({'resultado': 2})
+
+    def post(self, request):
+        dados = request  # recebe os dados que o cara enviou
+        json_data = json.loads(
+            dados.data)  # faz o parse de json para dicionario
+        # Aqui você pode fazer algo interessante com o dado, tipo salvar no db
+        return Response({'resultado': json_data})
+
+
+return2 = Return2.as_view()
